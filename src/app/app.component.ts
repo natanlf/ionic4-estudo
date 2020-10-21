4d7b4ce869ee032dd981948c552aa86fd17d3682
Natan <natanlaraferreira@gmail.com>
2020-10-21 11:24:10 -0300

    Config Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,7 +15,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private storage: Storage
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,23 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+
+    this.testeStorage();
+  }
+
+  testeStorage() {
+     // set a key/value
+    this.storage.set('name', 'Max');
+
+    // Or to get a key/value pair
+    this.storage.get('age').then((val) => {
+      console.log('Your age is', val);
+    });
+
+    // Or to get a key/value pair
+    this.storage.get('name').then((val) => {
+      console.log('Your name is', val);
     });
   }
 }
